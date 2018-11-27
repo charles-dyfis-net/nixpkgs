ec2e8bc32d0b09b72d391891925727b963aa6974
Charles Duffy <charles@dyfis.net>
2018-11-27 03:57:33 +0000

    Leave initial copy of bees running

--- nixos/tests/bees.nix
+++ nixos/tests/bees.nix
@@ -22,19 +22,15 @@ import ./make-test.nix ({ lib, ... }:
   };
 
   testScript = ''
-    $machine->succeed("systemctl stop beesd\@aux.service");
     $machine->succeed("dd if=/dev/urandom of=/home/dedup-me-1 bs=1M count=8");
     $machine->succeed("cp --reflink=never /home/dedup-me-1 /home/dedup-me-2");
     $machine->succeed("sync");
-
-    # must recognize these files as not sharing any space before we start bees
-    $machine->fail(q([[ $(btrfs fi du -s --raw /home/dedup-me-{1,2} | awk 'NR>1 { print $3 }' | grep -E '^0$' | wc -l) -eq 0 ]]));
     $machine->succeed("systemctl start beesd\@aux.service");
 
     # FIXME: Need to do a better job at polling
     $machine->succeed("sleep 10");
 
     # assert that "Set Shared" column is nonzero
-    $machine->succeed(q([[ $(btrfs fi du -s --raw /home/dedup-me-{1,2} | awk 'NR>1 { print $3 }' | grep -E '^0$' | wc -l) -eq 0 ]]));
+    $machine->succeed(q(max_tests=10; sleep_time=2; for ((i=0; i<max_tests; i++)); do [[ $(btrfs fi du -s --raw /home/dedup-me-{1,2} | awk 'NR>1 { print $3 }' | grep -E '^0$' | wc -l) -eq 0 ]] && exit 0; sleep "$sleep_time"; done));
   '';
 })
